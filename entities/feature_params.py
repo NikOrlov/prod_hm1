2bce77022cb92882ef643fb4146bc1209d58a33e
NikOrlov <nikovtb1@gmail.com>
2021-05-09 21:50:20 +0300

    refactor directories

--- entities/feature_params.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from typing import List
-
-@dataclass()
-class FeatureParams:
-    numerical_features: List[str]
-    categorical_features: List[str]
-    target_feature: str
